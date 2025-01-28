ff4a2e942b9dddceac02c8315b68ec3be3ba3b44
dawwed98 <wedrowskid@yahoo.com>
2025-01-28 02:33:11 +0100

    Delete inict.project.py

--- inict.project.py
+++
@@ -1,5 +0,0 @@
-Zakupy = {"Piekarnia":["chleb", "pączek", "bułki"], "Warzywniaka":["marchew", "seler", "rukola"]}
-print("Lista zakupów:")
-print("Piekarnia:", ", ".join(Zakupy["Piekarnia"]))
-print("Warzywniak:", ", ".join(Zakupy["Warzywniaka"]))
-print("w sumie kupuje", len(Zakupy["Piekarnia"]) + len(Zakupy["Warzywniaka"]), "produktów")

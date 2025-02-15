e64498e7c16078107ec59927c97b25a6017476eb
Dawid <wedrowskid@yahoo.com>
2025-02-15 01:27:39 +0100

    nie wiem co robie

--- zad mod5.2.py
+++ zad mod5.2.py
@@ -1,11 +1,27 @@
+from faker import Faker
+import random
+
+fake = Faker()
 class Film:
+    counter = 0
+    film_list = [
+       
+    ]
+    
     def __init__(self, tytuł, rok_wydania, gatunek, liczba_odtworzeń):
         self.tytuł = tytuł
         self.rok_wydania = rok_wydania
         self.gatunek = gatunek
         self.liczba_odtworzeń = liczba_odtworzeń
+        Film.counter += 1
+        Film.film_list.append(self)
+
     def __str__(self):
         return f"{self.tytuł} ({self.rok_wydania})"
+
+    def play(self):
+        self.liczba_odtworzeń += 1
+        return self.liczba_odtworzeń
 
 class Seriale(Film):
     def __init__(self, tytuł, rok_wydania, gatunek, liczba_odtworzeń, numer_sezonu, numer_odcinka):
@@ -15,7 +31,16 @@ class Seriale(Film):
     def __str__(self):
         return f"{self.tytuł} S{self.numer_sezonu}E{self.numer_odcinka}"
 
-object1 = Film("Wiedźmin", 2019, "Fantasy", 100)
-object2 = Seriale("Wiedźmin", 2019, "Fantasy", 100, 1, 1)
-print(object1)
-print(object2)
+    def play(self):
+        self.liczba_odtworzeń += 1
+        return self.liczba_odtworzeń
+
+for i in range(5):
+    film = Film(tytuł=fake.catch_phrase(), rok_wydania=random.randint(1900, 2021), gatunek=fake.word(), liczba_odtworzeń=random.randint(0, 100))
+    print(film)
+    print(film.play())
+for i in range(5):  
+    film = Seriale(tytuł=fake.catch_phrase(), rok_wydania=random.randint(1900, 2021), gatunek=fake.word(), liczba_odtworzeń=random.randint(0, 100), numer_sezonu=random.randint(1, 10), numer_odcinka=random.randint(1, 20))
+    print(film)
+    print(film.play())
+print(Film.counter)
